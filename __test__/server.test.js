5faa057c4343703d8989823a51a619421554ae3c
abu-nofal <anofal719@gmail.com>
2021-08-15 17:17:11 +0300

    lab 06 done 2

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -38,7 +38,7 @@ describe('my API Server', ()=> {
     });
 
     let obj={
-        username:'ahmad10',
+        username:'ahmad11',
         password:'test123'
     }
 
@@ -53,9 +53,9 @@ describe('my API Server', ()=> {
     it('Create a record using POST signin ', async () => {
         // let test=base64.encoded('ahmad01:test123')
        
-        const response = await request.post('/signin').set('Authorization', `Basic YWhtYWQxMDp0ZXN0MTIz`); // async
+        const response = await request.post('/signin').set('Authorization', `Basic YWhtYWQxMTp0ZXN0MTIz`); // async
         expect(response.status).toEqual(200);
-    expect(response.body.username).toBe('ahmad10');
+    expect(response.body.username).toBe('ahmad11');
       
     
     });
